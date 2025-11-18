63a1e7bdf424c9969ce870460f9061df2c8c6298
jancanare <jancanare@gmail.com>
2025-11-18 20:12:27 +0800

    Remove old grid and make new grid

--- sketch.js
+++ sketch.js
@@ -16,5 +16,17 @@ for (let i = 0; i < 16 * 16; i++) {
        while (isNaN(size) || size < 1 || size > 100) {
       size = Number(prompt("Please enter a valid number between 1 and 100:"));
 }
+container.innerHTML = "";
+   
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("newCell");
+    cell.style.width = `calc(100% / ${size})`;
+    cell.addEventListener('mouseenter', function() {
+      cell.style.backgroundColor = 'dodgerblue';
+    });
+    container.appendChild(cell);
+  }
+
 console.log(size);     
     });
